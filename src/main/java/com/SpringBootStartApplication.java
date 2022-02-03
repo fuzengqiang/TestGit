4470435dcd59677e1d6ff750a6c51569d4dc569e
fuzengqiang <2652272235@qq.com>
2022-02-03 16:44:34 +0800

    更新IDEA测试

--- src/main/java/com/SpringBootStartApplication.java
+++ src/main/java/com/SpringBootStartApplication.java
@@ -3,6 +3,6 @@ package com;
 public class SpringBootStartApplication {
     public static void main(String[] args) {
         System.out.println("Hello wold");
-        System.err.println("Hello wold2")
+        System.err.println("Hello wold2");
     }
 }
