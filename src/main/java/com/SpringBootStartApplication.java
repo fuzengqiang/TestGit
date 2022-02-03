f23e4a8223f7f7573aebd80e021b6dcaba58dc8e
fuzengqiang <84963791+fuzengqiang@users.noreply.github.com>
2022-02-03 16:36:43 +0800

    Update SpringBootStartApplication.java
    更新SpringBootStartApplication

--- src/main/java/com/SpringBootStartApplication.java
+++ src/main/java/com/SpringBootStartApplication.java
@@ -3,5 +3,6 @@ package com;
 public class SpringBootStartApplication {
     public static void main(String[] args) {
         System.out.println("Hello wold");
+        System.err.println("Hello wold2")
     }
 }
